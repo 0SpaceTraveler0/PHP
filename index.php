39dcc568c6ec4bf25204dbe5148d953668bf67dd
0SpaceTraveler0 <t34ilya2014@mail.ru>
2022-08-31 14:43:02 +0500

    Первые настройки

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>Простейший PHP-скрипт</title>
+    <meta charset='utf-8'>
+</head>
+<body>
+123
+<?php
+    echo "Hello world!";
+?>
+</body>
+</html>
